f5f83881086aa8a01ed99a56c5d10aaf8c2ecc78
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-05-16 21:03:57 -0300

    Add files via upload

---
+++ src/assets/palett.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const styles = StyleSheet.create({
+    wine: {
+        color: "#AF1439",
+    },
+    yellow: {
+        color: "#FF9A3E",
+    },
+    purple: {
+        color: "#7C00FF",
+    },
+    dark_purple: {
+        color: "#302238",
+    },
+    white: {
+        color: "#FAFAFA",
+    },
+    grey_white: {
+        color: "#F1EEEE",
+    },
+    grey_text: {
+        color: "#868686",
+    },
+});
+
+export default styles;
